193acababa7b650e252a4f120a04ed5dd96736f6
RyanMcArthur <47610962+RyanMcArthur@users.noreply.github.com>
2019-03-05 11:50:21 -0600

    Update message.rb

--- lib/letter_opener/message.rb
+++ lib/letter_opener/message.rb
@@ -57,7 +57,7 @@ module LetterOpener
     end
 
     def filepath
-      File.join(@location, "#{type}.html")
+      File.join(@location, "#{type}.html").sub('/mnt/c/', 'C://')
     end
 
     def content_type
